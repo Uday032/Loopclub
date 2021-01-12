f1e6f256687d99b668656c132f4584995d251f13
Uday Agarawl <rahul.uday07@gmail.com>
2021-01-12 20:43:20 +0530

    Revert "Pair Programming Session"

--- backend/core/models.py
+++ backend/core/models.py
@@ -4,5 +4,4 @@ import datetime
 # Create your models here.
 class Subscribers(models.Model):
     emails = models.EmailField(max_length=254)
-    age = models.IntegerField(default=0)
     date_added = models.DateField(default=datetime.date.today)
--- frontend/src/components/Forms/Subscriptions.js
+++ frontend/src/components/Forms/Subscriptions.js
@@ -11,15 +11,12 @@ export default class Subscriptions extends Component {
         super();
 
         this.handleEmailChange = this.handleEmailChange.bind(this);
-        this.handleAgeChange = this.handleAgeChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
 
         this.state= {
             'email': '',
-            'age': '',
             'error': '',
-            'success': '',
-            'ageerror': ''
+            'success': ''
         }
     }
 
@@ -29,28 +26,11 @@ export default class Subscriptions extends Component {
             email: e.target.value
         })
     }
-
-    handleAgeChange(e){
-        console.log(e.target.value);
-        let value = e.target.value;
-        if(value>0) {
-            this.setState({
-                age: e.target.value,
-                error: ''
-            })
-        } else {
-            this.setState({
-                age: '',
-                ageerror: 'Please Enter the Correct age'  
-            })
-        }
-    }
     
     handleSubmit(e){
         e.preventDefault();
         const data = {
-            "emails": this.state.email,
-            "age": this.state.age
+            "emails": this.state.email
         }
         instance.post('/core/subscribers/',data)
             .then(res=>{
@@ -58,9 +38,7 @@ export default class Subscriptions extends Component {
                 if(res.data.emails) {
                     this.setState({
                         success: "Thank You for Subscribing!!",
-                        error: '',
-                        email: '',
-                        age: ''
+                        error: ''
                     })
                 }
                 if(res.data.error){
@@ -94,28 +72,13 @@ export default class Subscriptions extends Component {
                             We'll never share your email with anyone else.
                         </Form.Text>
                     </Form.Group>
-
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Control 
-                            type="number" 
-                            pattern="[0-9]*"
-                            placeholder="Age" 
-                            onChange={this.handleAgeChange}
-                            value={this.state.age}
-                            required
-                        />
-                        <Form.Text>{this.state.ageerror}</Form.Text>
-                    </Form.Group>
-
-                    <Button type="submit" className="mb-2 form-submit-button">
+                    </Col>
+                    <Col xs="auto">
+                    <Button type="submit" className="mb-2">
                         Submit
                     </Button>
                     </Col>
-                    {/* <Col xs="auto">
-                    
-                    </Col> */}
                 </Form.Row>
-                
                 <Form.Row className="justify-content-center mb-3">
                     <Col xs="auto">
                         <span className="text-success">{this.state.success}</span>
